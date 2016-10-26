565bd372498c450c1a9fae7c616854dce23c4bf0
Bruce Armstrong <bruce@fortressofgeekdom.org>
2016-10-25 23:37:36 -0600

    Fix NPE when switching views.  Fix not setting coms when starting

--- ManualMouse/app/src/main/java/org/fog/manualmouse/CirclePadFragment.java
+++ ManualMouse/app/src/main/java/org/fog/manualmouse/CirclePadFragment.java
@@ -8,14 +8,16 @@ import android.view.ViewGroup;
 
 public class CirclePadFragment extends Fragment {
 
+    private View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.circle_layout, container, false);
+        view = inflater.inflate(R.layout.circle_layout, container, false);
+        return view;
     }
 
     public void setComs(MouseComs coms) {
-        View view = getView();
-        ((CirclePad) view.findViewById(R.id.circle_pad)).setComs(coms);
+        if (view != null)
+            ((CirclePad) view.findViewById(R.id.circle_pad)).setComs(coms);
     }
 }
 
